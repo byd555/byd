bdd495743de5f357fe61d55d3c9e94f557892040
byd555 <byd58114@gmail.com>
2026-09-09 14:04:21 +0100

    telegram

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -28,9 +28,9 @@ export default function Services() {
           </p>
         </div>
         <div className="details-block details-contact">
-          {/* <p className="details-text">
-            Email us on: <a href="mailto:byd55814@gmail.com" className="details-link">byd55814@gmail.com</a>
-          </p> */}
+          <p className="details-text">
+            Telegram Contact: <a href="t.me/+447756908526" className="details-link">+447756908526</a>
+          </p> 
            <p className="details-text">
             Email us on: <a href="mailto:iscm@bydglobalmotorcanada.ltd" className="details-link">byd-ca.com</a>
           </p>
