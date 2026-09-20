bb83a17d039390462b6ec24bc2d6fc104d553121
byd555 <byd58114@gmail.com>
2026-09-20 12:42:18 +0100

    Add WhatsApp contact information
    Updated contact information to include WhatsApp.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -29,7 +29,7 @@ export default function Services() {
         </div>
         <div className="details-block details-contact">
           <p className="details-text">
-            Telegram Contact: <a href="https://t.me/+447756908526" className="details-link">+447756908526</a>
+            Whatsapp Contact: <a href="https://wa.me/+447756908526" className="details-link">+447756908526</a>
 
           </p> 
            <p className="details-text">
